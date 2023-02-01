de844a51418d0c3ff8654a812ba9273e663e769f
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-02-01 19:27:49 +0530

    Update q1.c

--- ALGORITHMS LAB/LAB1/q1.c
+++ ALGORITHMS LAB/LAB1/q1.c
@@ -1,7 +1,72 @@
 #include<stdio.h>
-#include"tree.c"
+#include<stdlib.h>
 //-----------------------
-struct node * search(struct node * root,int key)
+struct node
+{
+int value;
+struct node *right;
+struct node *left;
+};
+//-----------------------
+struct node * getnode(int ele)
+{
+    struct node * temp=malloc(sizeof(struct node));
+    temp->value=ele;
+    temp->left=NULL;
+    temp->right=NULL;
+    return (temp);
+}
+//-----------------------
+void inorder(struct node* temp) 
+{
+    if(temp)
+    {
+        inorder(temp->left);
+        printf("%d ",temp->value);
+        inorder(temp->right);
+    }
+}
+//-----------------------
+void preorder(struct node* root)
+{
+    if(root)
+    {
+        printf("%d ",root->value);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+//-----------------------
+void postorder(struct node* root)
+{
+    if(root)
+    {        
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ",root->value);
+    }
+}
+//-----------------------
+struct node* create_bst(struct node*root,int ele)
+ {
+    if(root==NULL)
+    {
+        root=getnode(ele);
+        return root;
+    }
+    else
+    {
+        if (ele < root->value) 
+            root->left = create_bst(root->left, ele); 
+        else if(ele>root->value) 
+            root->right=create_bst(root->right,ele); 
+        else 
+            printf("duplicates not allowed\n");
+    }
+    return root;
+ }
+ //-----------------------
+ struct node * search(struct node * root,int key)
 {
     if(root==NULL)
     return NULL;
@@ -13,41 +78,60 @@ struct node * search(struct node * root,int key)
     return(search(root->right,key)); 
 }
 //-----------------------
-struct node* create_bst(struct node*root,int ele)
+struct node* insert(struct node *root,int key)
 {
-    if(root==NULL)
+    struct node*prev=NULL;
+    struct node*temp=root;
+    while(temp)
     {
-        root=getnode(ele);
-        return root;
-    }
-    else
-    {
-        if (ele < root->value)
-            root->left = create_bst(root->left, ele);
-        else if (ele > root->value)
-            root->right = create_bst(root->right, ele);
+        prev=temp;
+        if(key< temp->value)
+        temp=temp->left;
         else
-            printf("no");
+        temp=temp->right;
     }
-    return root;
+    struct node*new=getnode(key);
+    if(key<prev->value)
+    prev->left=new;
+    else
+    prev->right=new;
+    return(root);
 }
+//-----------------------
 void main()
 {
     struct node *root=NULL;
-    int ch,ele;
+    int ch,ele,key;
+    printf("enter 1 to start or continue creating tree,enter -1 to stop at any time.");
+    printf("enter choice:"); scanf("%d",&ch);
+    if(ch!=-1)
+    {
     do
     {
-        printf("enter data: "); scanf("%d",&ele);
-        root=create_bst(root,ele);
-        scanf("%d",&ch);
-    }while(ch!=-1);
-    int key;
-    inorder(root);
+            printf("enter data: ");
+            scanf("%d", &ele);
+            root = create_bst(root, ele);
+            printf("enter choice: ");
+            scanf("%d", &ch);
+    } while (ch != -1);
+    }
+    else
+    exit(0);
+    
+    printf("preorder traversal:\n"); preorder(root);
+    printf("\ninorder traversal:\n"); inorder(root);
+    printf("\npost order traversal:\n"); postorder(root);
+    
     printf("\nenter element to search for:");
     scanf("%d",&key);
-    struct node *keynode=iterative_search(root,key);
+    struct node *keynode=search(root,key);
     if(keynode==NULL)
-    printf("the element does not exist in tree");
+    {
+        root=insert(root,key);
+        printf("The element %d is added to the tree.",key);
+        printf("The new tree is:\n");
+        inorder(root);
+    }
     else
     printf("%d exists in the tree",keynode->value);
 }

40e6099e013180d485e1f05bc0b19aacf609d3dc
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-03-12 20:46:01 +0530

    Update lab3_18to20.sql

--- DBS/lab4/lab3_18to20.sql
+++ DBS/lab4/lab3_18to20.sql
@@ -3,7 +3,7 @@ L3_18)create view all_courses as
     where course_id in (select course_id from course where dept_name='Comp. Sci.')
     and semester='Fall' and year=2009;
 
-L3_19)select all_courses.course_id,course.title from all_courses join course on 	all_courses.course_id=course.course_id;
+L3_19)select all_courses.course_id,course.title from all_courses join course on all_courses.course_id=course.course_id;
 
 L3_20) create view department_total_salary (department_name,total) as
   	 select dept_name,sum(salary) from instructor group by dept_name;

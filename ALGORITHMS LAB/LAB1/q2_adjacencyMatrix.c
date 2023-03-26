3c44a8dceb65b3a9febf1df2eb305b630e32473c
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-03-26 18:52:34 +0530

    Update q2_adjacencyMatrix.c

--- ALGORITHMS LAB/LAB1/q2_adjacencyMatrix.c
+++ ALGORITHMS LAB/LAB1/q2_adjacencyMatrix.c
@@ -2,13 +2,13 @@
 void main()
 {
     int vertices,i,j;
-    printf("enter number of vertices:"); scanf("%d",&vertices);
+    printf("enter number of vertices:"); 
+    scanf("%d",&vertices);
     int adjacencyMatrix[vertices+1][vertices+1];
     adjacencyMatrix[0][0]=-1;
     printf("enter vertices:\n");
     for(j=1;j<vertices+1;j++)
     {
-        printf("matrix[0][%d]=",j);
         scanf("%d",&adjacencyMatrix[0][j]);
     }
     for(i=1;i<vertices+1;i++)
@@ -20,8 +20,14 @@ void main()
     {
         for(j=1;j<vertices+1;j++)
         {
+            jump:
             printf("between %d and %d:",adjacencyMatrix[0][i],adjacencyMatrix[j][0]);
             scanf("%d",&adjacencyMatrix[i][j]);
+            if(adjacencyMatrix[i][j]!=1 && adjacencyMatrix[i][j]!=0)
+            {
+                printf("invalid input enter again\n");
+                goto jump;
+            }
         }        
     }
     printf("adjacency matrix is:\n");

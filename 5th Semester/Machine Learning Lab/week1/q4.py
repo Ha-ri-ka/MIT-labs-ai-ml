8545f96ff542b2793d0e62cb5a7cbdac0f3671ea
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-08-02 19:57:12 +0530

    Update q4.py

--- 5th Semester/Machine Learning Lab/week1/q4.py
+++ 5th Semester/Machine Learning Lab/week1/q4.py
@@ -2,3 +2,10 @@
 import numpy as np
 Assign_4= np.arange(100).reshape(5,-1)
 Assign_4[:,0:4]
+
+#output
+#array([[ 0,  1,  2,  3],
+#      [20, 21, 22, 23],
+#      [40, 41, 42, 43],
+#      [60, 61, 62, 63],
+#      [80, 81, 82, 83]])

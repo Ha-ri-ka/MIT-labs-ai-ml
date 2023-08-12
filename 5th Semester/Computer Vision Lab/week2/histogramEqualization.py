99405d77a6467ea6ef223addb1345eb9cccf9104
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-08-12 21:30:12 +0530

    Update histogramEqualization.py

--- 5th Semester/Computer Vision Lab/week2/histogramEqualization.py
+++ 5th Semester/Computer Vision Lab/week2/histogramEqualization.py
@@ -7,11 +7,25 @@ img=cv.imread(path,0)
 equ=cv.equalizeHist(img)
 pair=np.hstack((img,equ))
 cv.imwrite('original vs equalized.jpg',pair)
-cv.waitKey(0)
 #ravel-->used to change a 2-dimensional array or a multi-dimensional array into a contiguous flattened array.
-plt.hist(img.ravel(),256,[0,256])
-plt.hist(equ.ravel(),256,[0,256])
-plt.title('histogram')
-plt.legend(['original','equalized'])
+plt.figure(figsize=(10, 8))
+
+plt.subplot(2, 2, 1)
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
+
+plt.subplot(2, 2, 2)
+plt.hist(img.ravel(),256,[0,256],color='pink')
+plt.title('Original Histogram')
+
+plt.subplot(2, 2, 3)
+plt.imshow(equ, cmap='gray')
+plt.title('Equalized Image')
+
+plt.subplot(2, 2, 4)
+plt.hist(equ.ravel(),256,[0,256],color='pink')
+plt.title('Equalized Histogram')
+
+plt.tight_layout()
 plt.show()
 

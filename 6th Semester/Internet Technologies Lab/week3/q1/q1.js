7e607c17676bd12e3ce86032bb4eeb052e0373f7
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2024-02-17 16:48:52 +0530

    Add files via upload

--- 6th Semester/Internet Technologies Lab/week3/q1/q1.js
+++ 6th Semester/Internet Technologies Lab/week3/q1/q1.js
@@ -1,6 +1,17 @@
-$(document).ready(function() {
-    $(".button").click(function(){
-        var lmargin = $(".container").attr("margin-left");
-        console.log("hello")
+$(document).ready(function(){
+    $('.btn').click(function(){
+        var txt=$('.btn').text()
+        if(txt=='CLICK TO MOVE RIGHT'){
+            $('.btn').text('CLICK TO MOVE LEFT');
+            $('.container').animate({
+            left:'500px'
+        });
+        }
+        else if(txt=='CLICK TO MOVE LEFT'){
+            $('.btn').text('CLICK TO MOVE RIGHT');
+            $('.container').animate({
+            left:'0px'
+        });
+        }
     })
 });

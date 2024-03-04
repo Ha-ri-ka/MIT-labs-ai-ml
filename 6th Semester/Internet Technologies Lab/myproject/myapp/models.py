ad968aac2b32dd9d018f94cf119c0eb54e5bdfb0
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2024-03-04 15:32:56 +0530

    Delete 6th Semester/Internet Technologies Lab/myproject directory

--- 6th Semester/Internet Technologies Lab/myproject/myapp/models.py
+++
@@ -1,4 +0,0 @@
-#create databse here. 
-from django.db import models
-
-# Create your models here.
--- 6th Semester/Internet Technologies Lab/myproject/question1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Question1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'question1'
